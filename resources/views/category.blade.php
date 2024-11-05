91ba39937e819c84a33e65a896bf2aee89ae40d7
Muhammad Danis Hadriansyah <mdanishadriansyah@gmail.com>
2024-11-05 16:49:49 +0700

    update dikit

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -21,13 +21,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
         <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
           <li class="nav-item"> 
-            <a class="nav-link" aria-current="page" href="{{ url('/') }}">Home</a> 
+            <a class="nav-link" aria-current="page" href="{{ url('/dex') }}">Home</a> 
           </li> 
           <li class="nav-item"> 
             <a class="nav-link" href="{{ url('category') }}">Kategori</a> 
-          </li> 
-          <li class="nav-item"> 
-            <a class="nav-link" href="#Pro">Promo</a> 
           </li> 
           <li class="nav-item"> 
             <a class="nav-link" href="#Ker">Keranjang</a> 
